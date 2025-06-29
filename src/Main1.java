9a621bb58e619576bb4c34fe55d8d768904b4451
EvgeniyGorbenko1 <evgeniy.gorbenko2@gmail.com>
2025-06-29 23:14:39 +0300

    Delete src directory

--- src/Main1.java
+++
@@ -1,8 +0,0 @@
-//class Main1 {
-//    public static void main(String[] args) {
-//        ATM atm = new ATM(10, 5, 2);
-//        atm.addMoney(5, 2, 1);
-//        atm.withdrawMoney(350);
-//
-//    }
-//}
